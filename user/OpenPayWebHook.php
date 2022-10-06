e9d287bf725d90e4a37a61afde090eddeb2cc4ec
Alex Delgado <arekkusu0792@gmail.com>
2022-10-06 16:30:17 -0500

    Update OpenPayWebHook.php
    add template for receiving openpay webhook

--- user/OpenPayWebHook.php
+++ user/OpenPayWebHook.php
@@ -2,20 +2,22 @@
 
 require_once('ArcadierApi.php');
 
-$clientId = '';
-$secretClient = '';
+$marketClientId = 'CLIENT ID';
+$marketSecretClient = 'CLIENT SECRET ID';
 
 $json = file_get_contents('php://input');
 $data = json_decode($json);
 
 try {
+    $response = array();
+
     //For more type of hooks please see https://documents.openpay.mx/docs/api/#objeto-webhook
     //For each payload please see how it looks in the Openpay dashboard
     switch ($data->type) {
         case "verification":
             //This is only execute one time in order to activate the webhook in OpenPay portal
-            $api = new ArcadierApi($clientId, $clientSecret);
-            $api->SendEmail('verification@openpay.com', 'to email', $data->verification_code, 'Verification WebHook');
+            $api = new ArcadierApi($marketClientId, $marketSecretClient);
+            $api->SendEmail('verification@arcadier.com', 'To email', $data->verification_code, 'Verification WebHook');
 
             break;
         case "charge.succeeded":
@@ -40,13 +42,23 @@ try {
             //Do Something
             break;
         default:
-            echo "No hay opcion configurada pero llego al webhook de Arcadier.";
-            //http_response_code(400);
-            break;
+            throw new InvalidArgumentException('Is not valid the specific option. Please check payload.', 400);
     }
+
+    $response["Result"] = true;
+    $response["Message"] = 'insert message to return';
+    //Add more if needed
+    echo json_encode($response);
+    // echo '{"Result":"some message here"}';
+
 }
 catch (Exception $ex) {
-    //echo $ex->getMessage();
+    $errorMsg = array();
+    $errorMsg["Result"] = False;
+    $errorMsg["Message"] = ($ex->getMessage());
+    $errorMsg["Code"] = $ex->getCode();
+    //Add more if needed
+    echo json_encode($errorMsg);
 }
 
 ?>
